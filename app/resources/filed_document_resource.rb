cf04bb0295f3ffe4e35ee427d575725a827c23e8
Brandon Cordell <brandon@brandoncordell.com>
2016-07-27 12:56:15 -0400

    Add a file download url and more data

--- app/resources/filed_document_resource.rb
+++ app/resources/filed_document_resource.rb
@@ -1,3 +1,15 @@
 class FiledDocumentResource < JSONAPI::Resource
-  attributes :id, :filename, :created, :modified, :filed
+  attributes :id, :filename, :download_url, :admin, :user, :scanned_location_id, :filed_location_id, :cat_1, :cat_2, :cat_3, :description, :entry_method, :last_activity_admin_id, :created, :modified, :filed
+
+  def download_url
+    "https://denver.atlasforworkforce.com/admin/filed_documents/view/#{@model.id}"
+  end
+
+  def admin
+    @model.admin_id
+  end
+
+  def user
+    @model.user_id
+  end
 end

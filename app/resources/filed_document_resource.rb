a88a8f91fd897fe46aa646d5fe2f6342ee3026d7
Brandon Cordell <brandon@brandoncordell.com>
2016-07-27 15:59:02 -0400

    Add a custom filter `created_since` that accepts a string of unix time

--- app/resources/filed_document_resource.rb
+++ app/resources/filed_document_resource.rb
@@ -1,5 +1,12 @@
 class FiledDocumentResource < JSONAPI::Resource
-  attributes :id, :filename, :download_url, :admin, :user, :scanned_location_id, :filed_location_id, :cat_1, :cat_2, :cat_3, :description, :entry_method, :last_activity_admin_id, :created, :modified, :filed
+  attributes :id, :filename, :download_url, :admin, :user, :scanned_location_id,
+    :filed_location_id, :cat_1, :cat_2, :cat_3, :description, :entry_method,
+    :last_activity_admin_id, :created, :modified, :filed
+
+  filter :created_since, apply: -> (records, value, options) do
+    db_time = Time.at(value.first.to_i).utc
+    records.where('filed_documents.created >= ?', db_time)
+  end
 
   def download_url
     "https://denver.atlasforworkforce.com/admin/filed_documents/view/#{@model.id}"
@@ -13,3 +20,4 @@ class FiledDocumentResource < JSONAPI::Resource
     @model.user_id
   end
 end
+

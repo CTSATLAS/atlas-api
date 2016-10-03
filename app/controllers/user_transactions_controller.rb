bcfa3a4b97c511e0d1dc4a168069380071633e7f
Brandon Cordell <brandon@brandoncordell.com>
2016-10-03 13:44:12 -0400

    Return 422 if the service code requested isn't found

--- app/controllers/user_transactions_controller.rb
+++ app/controllers/user_transactions_controller.rb
@@ -2,8 +2,23 @@ class UserTransactionsController < ApplicationController
   def create
     if service_code
       code = ServiceCode.where(service_code: service_code).first
-      params['data']['attributes']['details'] = "#{code.service_code} - #{code.title}"
-      params['data']['attributes'].delete('service_code')
+
+      if code.nil?
+        error = {
+          errors: [{
+            title: 'not found',
+            detail: 'service_code - not found',
+            code: 100,
+            status: 422
+          }]
+        }
+
+        render json: error, status: :unprocessable_entity
+        return
+      else
+        params['data']['attributes']['details'] = "#{code.service_code} - #{code.title}"
+        params['data']['attributes'].delete('service_code')
+      end
     end
 
     process_request

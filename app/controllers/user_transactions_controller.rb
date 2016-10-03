3b32e3a8db577a6f97dce0514439da5bf9f91e86
Brandon Cordell <brandon@brandoncordell.com>
2016-10-03 13:37:52 -0400

    Add service_code info to details if service_code exists in the request data

--- app/controllers/user_transactions_controller.rb
+++ app/controllers/user_transactions_controller.rb
@@ -1,2 +1,17 @@
 class UserTransactionsController < ApplicationController
+  def create
+    if service_code
+      code = ServiceCode.where(service_code: service_code).first
+      params['data']['attributes']['details'] = "#{code.service_code} - #{code.title}"
+      params['data']['attributes'].delete('service_code')
+    end
+
+    process_request
+  end
+
+  private
+
+  def service_code
+    params['data']['attributes']['service_code']
+  end
 end
